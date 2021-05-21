03b066485f9ba4f1b4363ca85c3a915e9994bf4c
Kleber Curvelo <kleber.sangalo@hotmail.com>
2021-05-21 11:32:43 -0300

    Adicionei as interfaces gráficas do programa.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from nfelib.v4_00 import leiauteNFe_sub as parser
 from tkinter import filedialog, Tk
-
+import psycopg2
+from JanelaConexaoBanco import JanelaConexaoBanco as credenciaisBanco
+import PySimpleGUI as sg
 
 def assinatura_sql(nomeTabela):
 
@@ -41,10 +43,12 @@ def adiciona_valores_invoices(file,nota):
     additional_data = nota.infNFe.infAdic.infCpl.replace("					","")
     arquivo_nfe = adiciona_arquivo_nfe(file)
 
-    return f'{created_at},{updated_at},{number},\'{model}\',\'{serie}\',\'{note_issue_date}\',\'{note_entry_date}\',{cfop},' \
+    sql = f'({created_at},{updated_at},{number},\'{model}\',\'{serie}\',\'{note_issue_date}\',\'{note_entry_date}\',{cfop},' \
            f'\'{nfe_key}\',\'{cnpj_emit}\',\'{cnpj_dest}\',{amount},{bc_icms_amount},{icms_amount},{bc_icms_st_amount},' \
            f'{icms_st_amount},{ecf_serial_number},\'{document_type}\',{ref_nfe},\'{operation_nature}\',\'{additional_data}\',' \
-           f'\'{arquivo_nfe}\''
+           f'\'{arquivo_nfe}\')'
+
+    return sql
 
 def adiciona_arquivo_nfe(file):
     arquivo = open(file, 'r')
@@ -58,6 +62,9 @@ def adiciona_arquivo_nfe(file):
 
 def adiciona_valores_items(nota):
     produtos = nota.infNFe.det
+
+    sql = ""
+    i = 0
     for produto in produtos:
         created_at = "now()"
         updated_at = "now()"
@@ -69,10 +76,15 @@ def adiciona_valores_items(nota):
         item_type_id = produto.prod.NCM
         description = produto.prod.xProd
         cfop = produto.prod.CFOP
+
         if(produto.imposto.ICMS.ICMS00 is not None):
             bc_icms_amount = produto.imposto.ICMS.ICMS00.vBC
             icms_aliquot = produto.imposto.ICMS.ICMS00.pICMS
             icms_amount = produto.imposto.ICMS.ICMS00.vICMS
+            bc_icms_st_amount = 0
+            icms_st_amount = 0
+            st_aliquot = 0
+            tax_situation = produto.imposto.ICMS.ICMS00.CST
 
         elif(produto.imposto.ICMS.ICMS10 is not None):
             bc_icms_amount = produto.imposto.ICMS.ICMS10.vBC
@@ -81,21 +93,53 @@ def adiciona_valores_items(nota):
             bc_icms_st_amount = produto.imposto.ICMS.ICMS10.vBCST
             st_aliquot = produto.imposto.ICMS.ICMS10.pICMSST
             icms_st_amount = produto.imposto.ICMS.ICMS10.vICMSST
+            tax_situation = produto.imposto.ICMS.ICMS10.CST
 
         elif(produto.imposto.ICMS.ICMS20 is not None):
             bc_icms_amount = produto.imposto.ICMS.ICMS20.vBC
             icms_aliquot = produto.imposto.ICMS.ICMS20.pICMS
             icms_amount = produto.imposto.ICMS.ICMS20.vICMS
+            bc_icms_st_amount = 0
+            icms_st_amount = 0
+            st_aliquot = 0
+            tax_situation = produto.imposto.ICMS.ICMS20.CST
 
         elif(produto.imposto.ICMS.ICMS30 is not None):
             bc_icms_st_amount = produto.imposto.ICMS.ICMS30.vBCST
             st_aliquot = produto.imposto.ICMS.ICMS30.pICMSST
             icms_st_amount = produto.imposto.ICMS.ICMS30.vICMSST
+            bc_icms_amount = 0
+            icms_aliquot = 0
+            icms_amount = 0
+            tax_situation = produto.imposto.ICMS.ICMS30.CST
+
+        elif (produto.impostos.ICMS.ICMS40 is not None):
+            bc_icms_amount = 0
+            icms_aliquot = 0
+            icms_amount = 0
+            bc_icms_st_amount = 0
+            icms_st_amount = 0
+            st_aliquot = 0
+            tax_situation = produto.imposto.ICMS.ICMS40.CEST
 
         elif(produto.impostos.ICMS.ICMS51 is not None):
             bc_icms_amount = produto.imposto.ICMS.ICMS51.vBC
             icms_aliquot = produto.imposto.ICMS.ICMS51.pICMS
             icms_amount = produto.imposto.ICMS.ICMS51.vICMS
+            bc_icms_amount = 0
+            icms_aliquot = 0
+            icms_amount = 0
+            tax_situation = produto.imposto.ICMS.ICMS51.CEST
+
+        elif (produto.impostos.ICMS.ICMS60 is not None):
+            bc_icms_amount = 0
+            icms_aliquot = 0
+            icms_amount = 0
+            bc_icms_st_amount = 0
+            icms_st_amount = 0
+            st_aliquot = 0
+            tax_situation = produto.imposto.ICMS.ICMS60.CEST
+
 
         elif(produto.impostos.ICMS.ICMS70 is not None):
             bc_icms_amount = produto.imposto.ICMS.ICMS70.vBC
@@ -104,6 +148,7 @@ def adiciona_valores_items(nota):
             bc_icms_st_amount = produto.imposto.ICMS.ICMS70.vBCST
             st_aliquot = produto.imposto.ICMS.ICMS70.pICMSST
             icms_st_amount = produto.imposto.ICMS.ICMS70.vICMSST
+            tax_situation = produto.imposto.ICMS.ICMS70.CEST
 
         elif(produto.impostos.ICMS.ICMS90 is not None):
             bc_icms_amount = produto.imposto.ICMS.ICMS90.vBC
@@ -112,6 +157,7 @@ def adiciona_valores_items(nota):
             bc_icms_st_amount = produto.imposto.ICMS.ICMS90.vBCST
             st_aliquot = produto.imposto.ICMS.ICMS90.pICMSST
             icms_st_amount = produto.imposto.ICMS.ICMS90.vICMSST
+            tax_situation = produto.imposto.ICMS.ICMS90.CEST
 
         elif(produto.impostos.ICMS.ICMSPart is not None):
             bc_icms_amount = produto.imposto.ICMS.ICMSPart.vBC
@@ -120,40 +166,103 @@ def adiciona_valores_items(nota):
             bc_icms_st_amount = produto.imposto.ICMS.ICMSPart.vBCST
             st_aliquot = produto.imposto.ICMS.ICMSPart.pICMSST
             icms_st_amount = produto.imposto.ICMS.ICMSPart.vICMSST
+            tax_situation = produto.imposto.ICMS.ICMSPart.CEST
 
         else:
-
-
-
+            bc_icms_amount = 0
+            icms_aliquot = 0
+            icms_amount = 0
+            bc_icms_st_amount = 0
+            icms_st_amount = 0
+            st_aliquot = 0
+            tax_situation = -1
 
         brute_total_value = produto.prod.vProd
+        item_sequential_number = (i+1)
+        cest = produto.prod.CEST
+        barcode = produto.prod.cEANTrib
+        framing_code = "null"
+        situation = 0
+        sql += f'(\'{created_at}\',\'{updated_at}\',\'{product_code}\',{quantity},{amount},\'{unit_measure}\',' \
+           f'\'{item_type}\',{item_type_id},\'{description}\',{cfop},{bc_icms_amount},{brute_total_value},' \
+           f'{icms_aliquot},{icms_amount},{bc_icms_st_amount},{st_aliquot},{icms_st_amount},{item_sequential_number},' \
+           f'{cest},\'{barcode}\',\'{framing_code}\',{situation},\'{tax_situation}\')'
+
+        if(i < (len(produtos) - 1)):
+            sql += ",\n"
+        i += 1
+
+    return sql.replace("None", "null")
 
 
-        item_sequential_number
-        cest
-        barcode
-        framing_code
-        situation
-        tax_situation
+
+def conectaBanco(db_host,db_nome,db_usuario,db_senha,db_porta):
+
+    conexao = psycopg2.connect(dbname=db_nome,user=db_usuario,password=db_senha,host=db_host,port=db_porta)
 
 
+    return conexao
 
+def insereBanco(conexao,sql):
 
+    cursor = conexao.cursor()
 
+    cursor.execute(sql)
+    conexao.commit()
 
+    cursor.close()
 
 
 if __name__ == '__main__':
     root = Tk()
     root.withdraw()
     file_path = filedialog.askopenfilenames()
+    janelaCredenciais = credenciaisBanco()
+    credenciais = janelaCredenciais.getCredenciais()
+    try:
+        conexao = conectaBanco(credenciais['host'], credenciais['nomeBanco'],
+                           credenciais['usuario'], credenciais['senha'],credenciais['porta'])
+    except:
+        sg.popup(f'Ocorreu um erro ao tentar conectar ao Banco de Dados.'
+                 f'Verifique se os dados foram digitados corretamente.')
+        exit(1)
+
+    inseridos = list()
+    errosNaConversao = list()
+    errosNaInsercao = list()
+
     for file in file_path:
-        nota = parser.parse(file)
-        # assinaturaInvoices = assinatura_sql("invoices")
-        # assinaturaItems = assinatura_sql("items")
-        # valoresInvoices = adiciona_valores_invoices(file,nota)
-        # sqlInvoices = (f'{assinaturaInvoices} ({valoresInvoices})')
-        print(nota.infNFe.det[0].imposto.ICMS.ICMS00)
+        try:
+            nota = parser.parse(file)
+        except:
+            errosNaConversao.append(file)
+            continue
+        assinaturaInvoices = assinatura_sql("invoices")
+        valoresInvoices = adiciona_valores_invoices(file,nota)
+        sqlInvoices = f'{assinaturaInvoices} {valoresInvoices}'
+
+        assinaturaItems = assinatura_sql("items")
+        valoresItems = adiciona_valores_items(nota)
+        sqlItems = f'{assinaturaItems} {valoresItems}'
+
+        try:
+            insereBanco(conexao,sqlInvoices)
+            insereBanco(conexao,sqlItems)
+            inseridos.append(nota.infNFe.Id)
+        except:
+            errosNaInsercao.append(nota.infNFe.Id)
+
+    if(len(errosNaInsercao) != 0):
+        sg.popup(f'{len(inseridos)} Arquivos inseridos com sucesso no Banco de Dados.\n'
+                 f'Ocorreram erros ao tentar adicionar os arquivos:\n {errosNaInsercao}\n no Banco de Dados.')
+    else:
+        sg.popup(f' {len(inseridos)} Arquivos inseridos com sucesso no Banco de Dados.')
+
+    if(len(errosNaConversao) != 0):
+        sg.popup(f'Ocorreram erros na conversão de alguns arquivos. \n'
+                 f'Certifique-se de que os arquivos: \n {errosNaConversao} \n são arquivos de Notas Fiscais Eletrônicas.')
+
+    conexao.close()
 
 
 
